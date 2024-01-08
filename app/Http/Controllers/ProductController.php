0a799df92a1c635dab51b0ffd1e5b77b4284ff89
Tiffany Wilson <tiffany.r.smi@gmail.com>
2024-01-08 10:05:58 -0500

    Saving changes and updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function displayProducts()
+    {
+        $products = Product::all(); // Fetch products from the 'products' table (adjust if needed)
+        return view('admin.scheduleA')->with('products', $products);
+    }
 }
